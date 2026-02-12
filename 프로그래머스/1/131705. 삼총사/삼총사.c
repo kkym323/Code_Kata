de07635527f2e8929b3f651dcdeccc12b18eadd5
kkym323 <kkym32387@gmail.com>
2026-02-12 09:13:37 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// number_len은 배열 number의 길이입니다.
+int solution(int number[], size_t number_len) {
+    int answer = 0;
+    int n = number_len;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (number[i] + number[j] + number[k] == 0)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
