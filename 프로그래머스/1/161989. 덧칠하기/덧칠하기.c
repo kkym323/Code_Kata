a63513e90a4d5b41317956c22fa4ec5490b9d4e9
kkym323 <kkym32387@gmail.com>
2026-03-16 09:15:15 +0900

    [level 1] Title: 덧칠하기, Time: 0.06 ms, Memory: 5.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// section_len은 배열 section의 길이입니다.
+int solution(int n, int m, int section[], size_t section_len) {
+    int answer = 0;
+    int end = 0;
+    
+    for (int i = 0; i < section_len; i++)
+    {
+        if (section[i] > end)
+        {
+            answer++;
+            end = section[i] + m - 1;
+        }
+    }
+    return answer;
+}
